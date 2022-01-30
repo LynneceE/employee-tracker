dd72c12f8e118e4d07fa346bdd7b8df8debd7b54
bhari eutsey <bharilynn@outlook.com>
2022-01-30 03:28:20 -0600

    added switch functions to scroll through options, add functions for each to return appropriate data

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ connection.connect(function(err) {
     }
    // confirm connection run npm start
     console.log('Connected!');
+
+    //employeePrompt(); = keeps the opening prompt visible so you dont have to quit and restart for a new prompt each time
+
     
     // start npm inquirer
  employeePrompt();
@@ -28,13 +31,145 @@ connection.connect(function(err) {
   function employeePrompt() {
       inquirer.prompt({
           type: 'list',
-          name: 'pick',
+          name: 'start',
           message: 'What would you like to do?',
           choices: [
               "View Employees",
+              "View Department",
               "View Roles",
-              "View Departments",
-              "View Employees by Department"
+              "View Employees by Department",
+              
+
+
+
           ]
-      })
+          // switch statements for choices
+      }).then(function({start}){
+          switch (start) {
+
+              case "View Employees":
+                  getEmployees();
+                  break;
+
+              case "View Department":
+                    getDepartment();
+                    break;
+                    
+              case "View Roles":
+                     getRoles();
+                     break;
+
+
+              case "View Employees by Department":
+                  getEmployeesByDepartment();
+                  break;
+
+               
+          }
+
+        
+
+      });
   }
+
+
+  // once a choice is made, display that data using sql joins (w3schools)
+      function getEmployees(){
+          console.log("--Employees--");
+          var query = "SELECT * FROM employee"
+          connection.query(query, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+
+            employeePrompt();
+        });
+        
+      }
+
+
+      function getDepartment(){
+          console.log("--Departments--");
+          var query = "SELECT * FROM department";
+          connection.query(query, function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            employeePrompt();
+          });
+          
+      }
+
+
+      function getRoles(){
+          console.log("--Roles--");
+          var query = "SELECT * FROM role";
+          connection.query(query, function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            employeePrompt();
+          });
+      }
+
+      function getEmployeesByDepartment(){
+          console.log("--By Department--");
+          var query =
+          `SELECT department.id, department.name  AS department
+           FROM employee employee
+           LEFT JOIN role role
+	       ON employee.role_id = role.id
+           LEFT JOIN department department
+           ON department.id = role.department_id
+           GROUP BY department.id, department.name`
+      
+        connection.query(query, function (err, res) {
+          if (err) throw err;
+              // create options from data table results
+              const getDepartment = res.map(data => ({
+                value: data.id, name: data.name
+              }));
+          console.table(res);
+
+         
+          
+          departmentPrompt(getDepartment);
+
+      });
+
+    }
+
+    function departmentPrompt(getDepartment) {
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: "departments",
+                message: "Choose a department",
+                choices: getDepartment
+            }
+        ]).then(function(answer) {
+            console.log("answer" + answer.departments);
+
+            var query =
+              `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department 
+               FROM employee employee
+               JOIN role role
+	           ON employee.role_id = role.id
+               JOIN department department
+               ON department.id = role.department_id
+               WHERE department.id = ?`
+
+      connection.query(query, answer.departments, function (err, res) {
+        if (err) throw err;
+
+        console.table("response ", res);
+
+        employeePrompt();
+        });
+
+    });
+    }
+
+    
+     
+
+
+
+    
